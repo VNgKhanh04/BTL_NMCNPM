eb071f299126055f677d72c38bd104c4de38dfa9
Hieun003 <hieun0981@gmail.com>
2025-03-04 21:41:00 +0700

    Add files via upload

---
+++ Models/Comment.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BTL_NMCNPM.Models{
+     public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nội dung bình luận không được để trống")]
+        [StringLength(500, ErrorMessage = "Bình luận không được vượt quá 500 ký tự")]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string ?UserId { get; set; }
+    }
+}
