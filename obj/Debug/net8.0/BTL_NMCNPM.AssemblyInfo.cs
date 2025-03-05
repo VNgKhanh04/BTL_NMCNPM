7bf2cda6eee1a22811e9da1b9e06ce9556bea1e7
VNgKhanh04 <vnk4070@gmail.com>
2025-03-05 13:44:14 +0700

    Revert "Add files via upload"
    This reverts commit 81b13acfa4a2bac3a6ae676f843026740528790b.

--- obj/Debug/net8.0/BTL_NMCNPM.AssemblyInfo.cs
+++ obj/Debug/net8.0/BTL_NMCNPM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_NMCNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635f52922ac7b6ef0875530da656ec8d85896fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_NMCNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_NMCNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
